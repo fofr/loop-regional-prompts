72d5fd0ce126f39fb66a986d1480ee580a59dc11
Paul Hayes <fofr@users.noreply.github.com>
2023-04-03 07:43:41 +0100

    Optimise the loop script

--- generate.rb
+++ generate.rb
@@ -1,35 +1,20 @@
 #!/usr/bin/env ruby
 # Distribution:
 # 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+steps = 20
 base_prompt = 'base prompt'
 list_of_prompts = [
   'prompt'
 ]
 
-steps = 20
-
 def loop_through_steps(base_prompt, start_prompt, end_prompt, steps)
-  prompts = []
-
-  (0...steps).each do |i|
-    p = Array.new(19 - i, start_prompt) + Array.new(i + 1, end_prompt)
-    prompts << "#{base_prompt} BREAK #{p.join(' BREAK ')}"
-  end
-
-  prompts
-end
-
-prompts = []
-list_of_prompts.each_with_index do |prompt, index|
-  if index < list_of_prompts.length - 1
-    prompts << loop_through_steps(base_prompt, prompt, list_of_prompts[index + 1], steps)
+  (0...steps).map do |i|
+    "#{base_prompt} BREAK #{Array.new(19 - i, start_prompt) + Array.new(i + 1, end_prompt)}"
   end
 end
 
-prompts.flatten!
-
-File.open('prompts.txt', 'w') do |f|
-  prompts.each do |p|
-    f.puts p
-  end
+prompts = list_of_prompts.each_cons(2).flat_map do |start_prompt, end_prompt|
+  loop_through_steps(base_prompt, start_prompt, end_prompt, steps)
 end
+
+File.write('prompts.txt', prompts.join("\n"))
